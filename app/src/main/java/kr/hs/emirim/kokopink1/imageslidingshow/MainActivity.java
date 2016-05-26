c489751a28f4f3ca7239ba386954627f9ea3796e
Eunji Shim <eunji991222@gmail.com>
2016-05-26 20:12:40 +0900

    #1 UI complete

--- app/src/main/java/kr/hs/emirim/kokopink1/imageslidingshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/imageslidingshow/MainActivity.java
@@ -2,12 +2,31 @@ package kr.hs.emirim.kokopink1.imageslidingshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip=(ViewFlipper)findViewById(R.id.view_flip);
+        flip.setFlipInterval(1000);
+        Button butStart=(Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        Button butStop=(Button)findViewById(R.id.but_stop);
+        butStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_start:
+                flip.startFlipping(); //이전화면
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping(); //다음화면
+                break;
+        }
     }
 }
